3218427b793e989d3d5e58fbd8e3b5f1fc05b2af
Raman <Raman@DESKTOP-UUFUMDH>
2022-04-16 00:13:45 -0400

    testing of operations

--- CalculatorTestProject/UnitTest1.cs
+++ CalculatorTestProject/UnitTest1.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using WebApplicationCalculatorAPI.Pages;
+using Xceed.Wpf.Toolkit;
 
 namespace CalculatorTestProject
 {
@@ -17,7 +18,35 @@ namespace CalculatorTestProject
             double number2 = 1;
             (double number1, double number2) p = (
                             number1, number2);
+            Assert.AreEqual(4, (object)Calculator.- p);
+
+        }
+        [Test]
+        public void Addition1()
+        {
+            double number1 = 5;
+            double number2 = 1;
+            (double number1, double number2) p = (
+                            number1, number2);
             Assert.AreEqual(4, (object)Calculator.+ p);
+        }
+        [Test]
+        public void Multiplication1()
+        {
+            double number1 = 5;
+            double number2 = 1;
+            (double number1, double number2) p = (
+                            number1, number2);
+            Assert.AreEqual(4, (object)Calculator.* p);
+        }
+        [Test]
+        public void Division1()
+        {
+            double number1 = 5;
+            double number2 = 1;
+            (double number1, double number2) p = (
+                            number1, number2);
+            Assert.AreEqual(4, (object)Calculator./ p);
         }
     }
 }

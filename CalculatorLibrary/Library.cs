1151f18c297f5b0f7c2d22942a141dd3f249bfb0
Raman <Raman@DESKTOP-UUFUMDH>
2022-04-17 10:11:46 -0400

    new code

---
+++ CalculatorLibrary/Library.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CalculatorLibrary
+{
+    public class Library
+    {
+        public static int Add(String series)
+        {
+            int result = 0;
+
+            String[] numbers = series.Split(' ');
+
+
+            foreach (string number in numbers)
+            {
+
+                int temp;
+
+
+                if (int.TryParse(number, out temp))
+                {
+
+                    result += temp;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+
+            }
+
+
+            return result;
+
+
+        }
+
+        public static double Sub(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+        public static double Multi(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+        public static double Division(double number1, double number2)
+        {
+            if (number2 != 0)
+            {
+                return number1 / number2;
+            }
+            else
+            {
+                return number1;
+            }
+        }
+        public static double Percentages(double number1, double number2)
+        {
+            return number1 % number2;
+            // return (number1 * number2) / 100;
+        }
+    }
+}
+   
--- CalculatorTestProject1/UnitTest1.cs
+++ CalculatorTestProject1/UnitTest1.cs
@@ -1,14 +1,154 @@
+using Bunit;
+using LINQtoCSV;
+using Microsoft.Extensions.DependencyModel;
+using NUnit.Framework;
 using System;
+using System.Collections.Generic;
+using Xceed.Wpf.Toolkit;
 using Xunit;
+using Assert = Xunit.Assert;
 
 namespace CalculatorTestProject1
 {
-    public class UnitTest1
+    public class UnitTest1 : Bunit.TestContext
     {
-        [Fact]
-        public void Test1()
-        {
+        private IEnumerable<object> expected;
 
+        [Fact]
+        [InlineData("5 1", 6)]
+       [InlineData ("10 20", 30)]
+        [InlineData("30 22", 52)]
+        [InlineData ("1 20", 21)]
+
+        public void Addition()
+        {
+            Assert.Equal(expected, Library.(input));
         }
+
+
+        [Test]
+        public void Subtraction1()
+        {
+            //double number1 = 5;
+           // double number2 = 1;
+            double result = Library.Sub(number1, number2);
+           Assert.Equal(4, result(2, 2));
+        }
+        [Test]
+        public void Subtraction2()
+        {
+            double number1 = 15;
+            double number2 = 5;
+            double result = Library.Sub(number1, number2);
+            Assert.Equal(10, result);
+        }
+
+        [Test]
+        public void Subtraction3()
+        {
+            double number1 = 30;
+            double number2 = 2;
+            double result = Library.Sub(number1, number2);
+           Assert.Equal(28, result);
+        }
+        [Test]
+        public void Subtraction4()
+        {
+            double number1 = 10;
+            double number2 = 11;
+            double result = Library.Sub(number1, number2);
+            Assert.Equal(-1, result);
+        }
+
+        [Test]
+        public void Multiplication1()
+        {
+            double number1 = 5;
+            double number2 = 1;
+            double result = Library.Multi(number1, number2);
+            Assert.Equal(5, result);
+        }
+
+        [Test]
+        public void Multiplication2()
+        {
+            double number1 = 5;
+            double number2 = 5;
+            double result = Library.Multi(number1, number2);
+          Assert.AreEqual(25, result);
+        }
+
+        [Test]
+        public void Multiplication3()
+        {
+            double number1 = 3;
+            double number2 = 2;
+            double result = Library.Multi(number1, number2);
+            Assert.Equal(6, result);
+        }
+        [Test]
+        public void Multiplication4()
+        {
+            double number1 = 6;
+            double number2 = 4;
+            double result = Library.Multi(number1, number2);
+            Assert.Equal(24, result);
+        }
+        [Test]
+        public void Division1()
+        {
+            double number1 = 10;
+            double number2 = 2;
+            double result = Library.Division(number1, number2);
+            Assert.Equal(5, result);
+        }
+
+        [Test]
+        public void Division2()
+        {
+            double number1 = 30;
+            double number2 = 6;
+            double result = Library.Division(number1, number2);
+           Assert.Equal(5, result);
+        }
+
+        [Test]
+        public void Division3()
+        {
+            double number1 = 10;
+            double number2 = 10;
+            double result = Library.Division(number1, number2);
+           Assert.Equal(1, result);
+        }
+        [Test]
+        public void Division4()
+        {
+            double number1 = 100;
+            double number2 = 10;
+            double result = Library.Division(number1, number2);
+            Assert.Equal(10, result);
+        }
+        [Test]
+        public void Percentage1()
+        {
+            double number1 = 400;
+            double number2 = 320;
+            double result = Library.Percentages(number1, number2);
+            Assert.Equal(80, result);
+        }
+
+        /* [Fact]
+         public void Test1()
+         {
+
+                 double number1 = 5;
+                 double number2 = 1;
+                 (double number1, double number2) p = (
+                                 number1, number2);
+                 Assert.AreEqual(4, (object)Calculator.+ p);
+             }
+
+         } */
     }
 }
+
